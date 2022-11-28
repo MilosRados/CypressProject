e22429cf804b84222fc13e32ed4f9e1e0c2d0b89
MilosRados <mickeyrados1@gmail.com>
2022-11-28 11:43:44 +0100

    Shopping Cart

---
+++ cypress/e2e/pages/shopping_cart.js
@@ -0,0 +1,25 @@
+export class ShoppingCart{
+    
+                  
+
+     clickShoppCart(){
+        cy.contains('Shopping cart').click()
+     }       
+     assertTitle(){
+        cy.contains('h1','Shopping cart').should('have.text','Shopping cart')
+     }
+     assertProdImg(){
+        cy.get('img[alt="Picture of HTC One M8 Android L 5.0 Lollipop"]').should('be.visible')
+
+     }
+     selectGiftW(){
+        cy.get('#checkout_attribute_1').select('2').should('have.value','2')
+     }
+     clickChecbox(){
+        cy.contains('.terms-of-service','I agree with the terms of service and I adhere to them unconditionally').find('[type="checkbox"]').then(checkBox => {
+            cy.wrap(checkBox)
+            .check()
+            })
+     }
+
+}
